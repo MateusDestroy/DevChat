abc5166241b35626062a7f9f0b6508e4b44b9b76
MateusDestroy <mateuseustaquiomm@gmail.com>
2023-01-21 14:03:16 -0300

    foi

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -1,32 +1,19 @@
-import "./styled.scss"
-import Cabecalho from "../../components/chat/cabecalho"
+import "./styled.scss";
+import Cabecalho from "../../components/chat/cabecalho";
 
-export default function Chat(){
-    return(
-        <div className="conteiner-chat">
-            <Cabecalho></Cabecalho>
-            <div className="conteiner-mensagens-chat">
-
-                <div className="mensagens-chat">
-                
-                </div>
-
-
-                <div className="buttons-inputs-chat">
-
-                    
-
-                </div>
-
-
-
-            </div>
-
-         
+export default function Chat() {
+  return (
+    <div className="conteiner-chat">
+      <Cabecalho></Cabecalho>
+      <div className="conteiner-mensagens-chat">
+        <div className="chat">
+          <div className="mensagens-chat">
+            <div className="mensagens"></div>
+          </div>
         </div>
 
-
-
-  
-    )
+        <div className="buttons-inputs-chat"></div>
+      </div>
+    </div>
+  );
 }
